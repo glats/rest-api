0ec3d65f5c58b769179cac3bfafbba566b430a7a
glats <juan.cuzmar.s@gmail.com>
2018-05-24 17:30:21 -0400

    adding cords for test

--- main.go
+++ main.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 
 	"github.com/glats/rest-api/app/contact"
-	"github.com/gorilla/handlers"
+    "github.com/rs/cors"
 	"github.com/gorilla/mux"
 )
 
 // our main function
 func main() {
-	corsObj := handlers.AllowedOrigins([]string{"*"})
 	router := mux.NewRouter()
 	router.HandleFunc("/contact", contact.RecieveContact).Methods("POST")
-	log.Printf("%v", http.ListenAndServe(":8080", handlers.CORS(corsObj)(router)))
+    handler := cors.Default().Handler(router)
+	log.Printf("%v", http.ListenAndServe(":8080", handler))
 }
